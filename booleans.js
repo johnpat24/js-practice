cedb435fbb1e802c1cb452b8c6a56cd49b111927
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-15 13:52:24 +0800

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,8 @@
+// variables
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 90;
+let add = favNumb + undefined;
+
+// to print
+console.log(isJsProgrammingLanguage + "\n" + isJsHard + "\n" + favNumb + "\n" + add)
