054118da18e0c19153f96057ae85f313c71cb6d1
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-15 13:51:30 +0800

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,7 @@
+// input variable
+let name = " John Patrick Andaquig";
+let whatDoYouWannaBecomeInYourLife = " Software Engineer";
+let gender = " Male";
+let twitterHandle = " N/A";
+// printing the output
+console.log(name + "\n" + whatDoYouWannaBecomeInYourLife + "\n" + gender + "\n" + twitterHandle )
