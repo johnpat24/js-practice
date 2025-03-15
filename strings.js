10cf9c26d16cca6a75b97571d231ffa8fe7641f1
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-15 13:53:14 +0800

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,14 @@
+// variables
+let favActorFirstName = "Tom";
+let favActorLastName = "Holland";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = "my favorite actor is " + uppercase;
+message += " his best show is silicon valley"
+
+//print
+console.log(favActorFirstName)
+console.log(favActorLastName)
+console.log(fullName)
+console.log(uppercase)
+console.log(message)
