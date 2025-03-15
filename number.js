4246046d9705506ad1f94619d1dc08bc20e21e23
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-03-15 13:51:57 +0800

    Add files via upload

---
+++ number.js
@@ -0,0 +1,14 @@
+// variables
+let firstFavNum = 90
+let secondFavNum = 110
+
+// performing operations 
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulos = firstFavNum % secondFavNum;
+let exponentation = firstFavNum ** secondFavNum;
+
+// printing the output
+console.log("Addition: " + addition + "\n" + "Subtraction: " + subtraction + "\n" + "Multiplication: " + multiplication + "\n" + "Division:" + division + "\n" + "Modulos: " + modulos + "\n" + "Exponentation(Power): " + exponentation)
